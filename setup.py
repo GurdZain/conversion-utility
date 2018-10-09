eb6cffe6768475f16022e4f82616d8eb4a6f7870
zhaojk <zhaojk@baichengtuan.com>
2018-10-09 18:11:12 +0800

    setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='conversions',
-    version='0.0.1',
+    version='0.0.2',
     author='gurdzain',
     author_email='spring.py.k@gmail.com',
-    url='',
+    url='https://github.com/GurdZain/conversion-utility',
     description=u'',
     packages=['conversions'],
 )
